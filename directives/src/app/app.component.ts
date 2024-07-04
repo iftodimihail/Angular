5950842fddc7a54b5f0200ecc7627b2027047164
Mihail Iftodi <mi250228@ncr.com>
2024-07-04 13:43:40 +0300

    add directives project

---
+++ directives/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component, computed, inject } from '@angular/core';
+
+import { AuthComponent } from './auth/auth.component';
+import { AuthDirective } from './auth/auth.directive';
+import { AuthService } from './auth/auth.service';
+import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
+import { LogDirective } from './log.directive';
+import { NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [AuthComponent, LearningResourcesComponent, NgIf, AuthDirective],
+  hostDirectives: [LogDirective],
+})
+export class AppComponent {
+  private authService = inject(AuthService);
+
+  isAdmin = computed(() => this.authService.activePermission() === 'admin');
+}
---
+++ directives/src/app/auth/auth.directive.ts
@@ -0,0 +1,32 @@
+import {
+  Directive,
+  TemplateRef,
+  ViewContainerRef,
+  effect,
+  inject,
+  input,
+} from '@angular/core';
+
+import { AuthService } from './auth.service';
+import { Permission } from './auth.model';
+
+@Directive({
+  selector: '[appAuth]',
+  standalone: true,
+})
+export class AuthDirective {
+  userType = input.required<Permission>({ alias: 'appAuth' });
+  private authService = inject(AuthService);
+  private templateRef = inject(TemplateRef);
+  private viewContainerRef = inject(ViewContainerRef);
+
+  constructor() {
+    effect(() => {
+      if (this.authService.activePermission() === this.userType()) {
+        this.viewContainerRef.createEmbeddedView(this.templateRef);
+      } else {
+        this.viewContainerRef.clear();
+      }
+    });
+  }
+}
